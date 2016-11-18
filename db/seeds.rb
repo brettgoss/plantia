b73897a65b3ccf25594d54c03c2889cf1d186971
Aileen O'Brien <aileentobrien@gmail.com>
2016-11-17 20:31:44 -0800

    Create seed data for Users and Plants
    Add lots of people and plants to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,90 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding Data..."
+
+raise "Development seeds only (for now)!" unless Rails.env.development?
+
+puts "Creating Users"
+
+User.create!({
+  name: "Aileen",
+  email: "aileen@fake.com",
+  password: "aileen",
+  })
+
+User.create!({
+  name: "Brett",
+  email: "brett@fake.com",
+  password: "brett",
+  })
+
+User.create!({
+  name: "Kim",
+  email: "kim@fake.com",
+  password: "kim",
+  })
+
+User.create!({
+  name: "Linley",
+  email: "linley@fake.com",
+  password: "linley",
+  })
+
+puts "Seeding Plants!(LOL!!)"
+
+Plant.create!({
+  nickname:  "Mittens",
+  common_name: "Venus Flytrap",
+  light: "Direct Sunlight",
+  water_freq: 4,
+  user_id: 1
+})
+
+Plant.create!({
+  nickname:  "Fernie Sanders",
+  common_name: "Maiden Hair Fern",
+  light: "Indirect Sunlight",
+  water_freq: 1,
+  user_id: 1
+})
+
+Plant.create!({
+  nickname:  "Sparky",
+  common_name: "Succulent",
+  light: "Direct Sunlight",
+  water_freq: 5,
+  user_id: 1
+})
+
+Plant.create!({
+  nickname:  "Finnegan",
+  common_name: "Norfolk Pine",
+  light: "Indirect Sunlight",
+  water_freq: 7,
+  user_id: 1
+})
+
+Plant.create!({
+  nickname:  "Frankie",
+  common_name: "Ficus",
+  light: "Direct Sunlight",
+  water_freq: 7,
+  user_id: 2
+})
+
+Plant.create!({
+  nickname:  "Kaa",
+  common_name: "Snake Plant",
+  light: "Direct Sunlight",
+  water_freq: 14,
+  user_id: 3
+})
+
+Plant.create!({
+  nickname:  "Linley jr.",
+  common_name: "Aloe",
+  light: "Indirect Sunlight",
+  water_freq: 7,
+  user_id: 4
+})
