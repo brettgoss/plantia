3ad54fff9f0343285577853c40b3d79c400c7caa
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-11-19 21:57:21 +0000

    Add Gems for Rspec and Testing
    Add these dependencies and wrote a few validations with a testing script.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
+has_secure_password
 
-  has_secure_password
+validates :name, presence: true,
+                length: { minimum: 2 }
+
+validates :email, presence: true,
+                confirmation: { case_sensitive: false }
+
+validates :password, presence: true,
+                length: { in: 4..20 }
 
 end
