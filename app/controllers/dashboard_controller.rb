68ff04d9ca6c5ec60351d2b38aeb4e698ee7ef82
Brett Goss <brettgoss@live.com>
2016-11-19 10:58:44 -0800

    Display only plants associated with current_user in dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < ApplicationController
   before_action :authorize
 
   def index
-    @plants = Plant.all
+    puts(current_user.id)
+    @user = User.find(current_user.id)
+    @plants = @user.plants.all
   end
 end
