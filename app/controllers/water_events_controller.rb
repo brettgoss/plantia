cf8aacbd2a8c7db4c342acba31dcb62cc9b26707
Aileen O'Brien <aileentobrien@gmail.com>
2016-11-22 19:15:30 -0800

    Add water all button
    Add routes for water all
    split create water event into two methods
    -one for watering one plant
    -one for watering all plants
    Add button to view

--- app/controllers/water_events_controller.rb
+++ app/controllers/water_events_controller.rb
@@ -1,9 +1,8 @@
 class WaterEventsController < ApplicationController
 
   def create
-    e = WaterEvent.new(water_event_params)
-    e.water_date = Time.now.utc
-    if e.save
+    plant_id = params[:plant_id]
+    if create_water_event(plant_id)
       render json: e
     else
       render json: e.errors, status: :unprocessable_entity
@@ -16,7 +15,35 @@ class WaterEventsController < ApplicationController
     head :no_content
   end
 
+  def create_water_event(plant_id)
+    plant_hash = {plant_id: plant_id}
+    e = WaterEvent.new(plant_hash)
+    e.water_date = Time.now.utc
+    return e.save
+  end
+
+  def water_all
+    plantaroos = current_user.plants.all
+    failed_plantaroos = []
+
+    plantaroos.each do |plant|
+      if !create_water_event(plant.id)
+        failed_plantaroos.push(plant)
+      end
+    end
+
+    if !failed_plantaroos.empty?
+      puts "VIOLENT EEEEEENDS" #IN FUTURE TELL USER ABOUT THESE FAILED PLANTS
+    end
+  end
+
+
+
   private
+
+  #   def water_all_params
+  #   params.require('/water_all').permit(:user_id)
+  # end
 
   def water_event_params
     params.require(:water_event).permit(:plant_id, :water_date)
