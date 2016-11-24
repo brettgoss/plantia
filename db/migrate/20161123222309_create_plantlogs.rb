8a38fc0dc18b6b93a7004cde6e001dae38d5c517
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-11-24 19:45:14 +0000

    Refractor datetime to date for efficiency and DRY
    Change table value "logtime" from datetime to date. Drop database, recreate database, migrate and seed.
    Show date with not time value.

--- db/migrate/20161123222309_create_plantlogs.rb
+++ db/migrate/20161123222309_create_plantlogs.rb
@@ -1,7 +1,7 @@
 class CreatePlantlogs < ActiveRecord::Migration[5.0]
   def change
     create_table :plantlogs do |t|
-      t.datetime :logtime
+      t.date :logtime
       t.string :title
       t.text :logtext
       t.references :plant, foreign_key: true
