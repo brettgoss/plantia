fa6b575413f9788961b0c1373094c63b65bf55cf
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-11-24 02:21:39 +0000

    Work on Plant log functionality
    Make plantlog views work with plant log controller.
    Routes issue does not allow plantlogs to be saved.

--- app/controllers/plantlogs_controller.rb
+++ app/controllers/plantlogs_controller.rb
@@ -1,20 +1,21 @@
 class PlantlogsController < ApplicationController
 
   def create
+    logger.error "In plantlog controller"
     @plant = Plant.find(params[:plant_id])
-    @plantlog = @plant.plantlogs.create(plantlog_params)
+    @plantlog = @plant.plantlogs.new(plantlog_params)
 
       if @plantlog.save!
       redirect_to @plant
     else
-      redirect_to @plant
+      redirect_to '/'
     end
     # redirect_to plant_path(@plant)
   end
 
   private
-    def comment_params
-      params.require(:plantlog).permit(:logtime, :text, :title)
+    def plantlog_params
+      params.require(:plantlog).permit(:logtime, :title, :logtext, :plant_id)
     end
 end
 end
