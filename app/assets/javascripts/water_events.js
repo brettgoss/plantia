3ff109a8c02b8c5b90591796000ae98e990c555c
Brett Goss <brettgoss@live.com>
2016-11-24 16:37:18 -0800

    Change template literal string to use string concatination.
    When precompiling assets for production, the template literals cause a
    crash. String concatination avoids this issue.

--- app/assets/javascripts/water_events.js
+++ app/assets/javascripts/water_events.js
@@ -3,12 +3,12 @@
 
 $(document).on("ajax:success", "form", function(status, data, xhr){
   if (data.plant_id === 0) {
-    $(`.message${data.plant_id}`).text("All your plants have been watered!");
-    $(`.message${data.plant_id}`).show().delay(1000).fadeOut();
+    $('.message' + data.plant_id).text("All your plants have been watered!");
+    $('.message' + data.plant_id).show().delay(1000).fadeOut();
   }
   else {
-    $(`.message${data.plant_id}`).text("Plant Watered!");
-    $(`.message${data.plant_id}`).show().delay(1000).fadeOut();
+    $('.message' + data.plant_id).text("Plant Watered!");
+    $('.message' + data.plant_id).show().delay(1000).fadeOut();
   }
 
    console.log('status');
