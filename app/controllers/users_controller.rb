b5203ddf2725d48123cf598333a25129bf616a81
Aileen O'Brien <aileentobrien@gmail.com>
2016-11-26 15:45:28 -0800

    WIP welcome styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = user.id
       redirect_to '/dashboard' #redirectooo to dashboard in future
     else
-      redirect_to '/' #redirect welcome page in future
+      render :new
     end
   end
 
