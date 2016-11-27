33eb863f16f28e0a605b23f7da6540d7c243ca19
Aileen O'Brien <aileentobrien@gmail.com>
2016-11-27 12:46:54 -0800

    User registration to display error messages
    -on unsuccesful signup, display appropriate error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+
   def new
     @user = User.new
   end
@@ -7,10 +10,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/dashboard' #redirectooo to dashboard in future
     else
-      render :new
+      render '/welcome/index'
     end
   end
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,27 @@
 class WelcomeController < ApplicationController
+
   def index
+    @user = User.new
   end
-end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/dashboard' #redirectooo to dashboard in future
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  end
+
