2676402f1b74e094cface6f7af4b9019b0c2573f
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-11-28 06:54:14 +0000

    Add column to users table to collect phone numbers
    Migrate new column to table and reset database.
    New column is present in the schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123222309) do
+ActiveRecord::Schema.define(version: 20161128055121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20161123222309) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "phone_number"
   end
 
   create_table "water_events", force: :cascade do |t|
