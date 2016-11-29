6edca6b6f0a16a20910caea87f28b2f470de9e86
Brett Goss <brettgoss@live.com>
2016-11-28 16:53:12 -0800

    Refactor controllers
      - Add comments to describe methods
      - Delete obsolete code
      - Remove old commented code
      - Rename variables to be more logical

--- app/controllers/water_events_controller.rb
+++ app/controllers/water_events_controller.rb
@@ -1,5 +1,6 @@
 class WaterEventsController < ApplicationController
 
+  # Handles request to water one plant
   def create
     plant_id = params[:plant_id]
     e = create_water_event(plant_id)
@@ -13,15 +14,28 @@ class WaterEventsController < ApplicationController
     end
   end
 
-  def destroy
-    @water_event = WaterEvent.find(params[:id])
-    # @water_event = WaterEvent.find params[:id]
-    @water_event.destroy
-    redirect_to plant_path(@water_event.plant)
+  # Handles request to water all plants
+  def water_all
+    @plants = current_user.plants.all
+    failed_plants = []
+    success_plants = []
+
+    @plants.each do |plant|
+      if create_water_event(plant.id.to_s).save
+        success_plants.push(plant)
+      else
+        failed_plants.push(plant)
+      end
+    end
+
+    if !success_plants.empty?
+      response = WaterEvent.where(plant_id: @plants.ids)
+      render json: response, status: :ok
+    end
   end
 
+  # Attempt to create new water event
   def create_water_event(plant_id)
-
     plant_hash = { plant_id: plant_id}
     e = WaterEvent.new(plant_hash)
     @plant = Plant.find(plant_id)
@@ -31,32 +45,16 @@ class WaterEventsController < ApplicationController
     e
   end
 
-  def water_all
-    plants = current_user.plants.all
-    failed_plants = []
-    success_plants = []
-
-    plants.each do |plant|
-      if create_water_event(plant.id.to_s).save
-        success_plants.push(plant)
-      else
-        failed_plants.push(plant)
-      end
-
-    end
-
-    if !success_plants.empty?
-        response = WaterEvent.where(plant_id: plants.ids)
-        render json: response, status: :ok
-    end
+  # Handles deletion of a water event
+  def destroy
+    @water_event = WaterEvent.find(params[:id])
+    @water_event.destroy
+    redirect_to plant_path(@water_event.plant)
   end
 
 
-
-  private
-
+private
   def water_event_params
     params.require(:water_event).permit(:plant_id, :water_date)
   end
-
 end
