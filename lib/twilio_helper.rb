b31fd181d8d3f894d89a5db5c6b3084fb9ae207a
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-11-30 20:06:47 +0000

    Take out plant id variable from  twilio helper
    Schema is now the same as group.

--- lib/twilio_helper.rb
+++ lib/twilio_helper.rb
@@ -5,13 +5,11 @@ class TwilioHelper
     client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
     phone_number = User.phone_number
     # Trying to call on the specific plant belongs to the user that needs to be watered.
-    plant = Plant.id.where(Plant.user_id == User_id)
     # Trying to include plant nickname and commonname in sms.
     #Maybe try a blanket text first ex: Hey one of your plants needs watering please check Plantia.
     alert_message = <<MSG
                     [This is a test] ALERT!
-                    Time to water your plant!
-                    #{plant_nickname} #{plant_common_name}
+                    One or more of your green babies needs some love.
                     Go to: https://plantia.herokuapp.com/ for more details."
                     MSG
 
