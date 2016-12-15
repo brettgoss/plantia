8dd1601155ac37a42e61e7ebd90baba3e03b7170
Brett Goss <brettgoss@live.com>
2016-12-15 15:23:30 -0800

    Add user profile route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   # These routes will be for signup. The first renders a form in the browse, the second will
   # receive the form and create a user in our database using the data given to us by the user.
     get '/welcome' => 'users#new'
+    get '/me' => 'users#index'
     post '/users' => 'users#create'
     put '/users' => 'users#update'
 
