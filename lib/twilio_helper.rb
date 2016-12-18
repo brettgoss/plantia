a6917a46e628fcecc5ddbef05630db33cecc85e8
Brett Goss <brettgoss@live.com>
2016-12-17 20:27:22 -0800

    Only try to send text message if user actually has a phone number saved

--- lib/twilio_helper.rb
+++ lib/twilio_helper.rb
@@ -2,17 +2,19 @@ class TwilioHelper
   require 'twilio-ruby'
 
   def send_text(user)
-     phone_number = user.phone_number
-     twilio_number = ENV["TWILIO_NUMBER"]
-    @client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
-     puts phone_number
-    alert_message = "One or more of your green babies needs some love. Go to: https://plantia.herokuapp.com/ for more details."
-    puts "sending_text"
-    @client.account.sms.messages.create(
-      :from => twilio_number,
-      :to =>  phone_number,
-      :body => alert_message,
-    )
-    puts "text sent"
+    if user.phone_number
+      phone_number = user.phone_number
+      twilio_number = ENV["TWILIO_NUMBER"]
+      @client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+      alert_message = "Your plants need watering! Go to: http://plantia.io for more details."
+      puts user.phone_number
+      puts "sending text"
+      @client.account.messages.create(
+        :from => twilio_number,
+        :to => phone_number,
+        :body => alert_message
+      )
+      puts "text sent"
+    end
   end
 end
