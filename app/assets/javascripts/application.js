6a4a6703c809c7d0f14f6965ce3bb36e37148bf7
Brett Goss <goss.brett@gmail.com>
2017-11-27 11:03:21 -0800

    Require serviceworker in pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,20 @@
 //= require react
 //= require react_ujs
 //= require components
+//= require serviceworker-companion
 //= require_tree .
+//= stub "serviceworker.js"
+
+if ('serviceWorker' in navigator) {
+  console.log('Service Worker is supported');
+  navigator.serviceWorker.register('/serviceworker.js')
+    .then(function (registration) {
+      console.log('Successfully registered!', ':^)', registration);
+      registration.pushManager.subscribe({ userVisibleOnly: true })
+        .then(function (subscription) {
+          console.log('endpoint:', subscription.endpoint);
+        });
+    }).catch(function (error) {
+      console.log('Registration failed', ':^(', error);
+    });
+}
