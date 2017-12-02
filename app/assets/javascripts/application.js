bb525a7cb6950fc74142ed72bb939ddec535854c
Brett Goss <goss.brett@gmail.com>
2017-12-01 23:15:33 -0800

    Register service worker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,11 +25,25 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/serviceworker.js')
     .then(function (registration) {
       console.log('Successfully registered!', ':^)', registration);
-      registration.pushManager.subscribe({ userVisibleOnly: true })
-        .then(function (subscription) {
-          console.log('endpoint:', subscription.endpoint);
-        });
+      navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
+        serviceWorkerRegistration.pushManager
+          .subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: window.vapidPublicKey
+          });
+      })
     }).catch(function (error) {
       console.log('Registration failed', ':^(', error);
     });
+}
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/serviceworker.js')
+    .then(function (reg) {
+      console.log('Service worker change, registered the service worker');
+    });
+}
+// Otherwise, no push notifications :(
+else {
+  console.error('Service worker is not supported in this browser');
 }
