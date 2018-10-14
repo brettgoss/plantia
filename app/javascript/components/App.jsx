c748b6630ca9558dbda49c633f43f6e63b8d20f5
Brett Goss <goss.brett@gmail.com>
2018-10-14 09:56:51 -0700

    Further refactor watering functions

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -10,7 +10,7 @@ class App extends React.Component {
   constructor(props) {
     super(props)
 
-    this.waterPlants = this.waterPlants.bind(this)
+    this.handlePlantWatering = this.handlePlantWatering.bind(this)
 
     this.state = {
       plants: this.props.plants,
@@ -19,20 +19,32 @@ class App extends React.Component {
     }
   }
 
+  handlePlantWatering (plantId) {
+    let messageText = ''
+    plantId.length
+      ? messageText = "Plant Watered!"
+      : messageText = "All your plants have been watered!"
+
+    this.waterPlants(plantId)
+      .then((response) => {
+        this.setState({
+          waterEvents: response,
+          messageText: messageText,
+        });
+      })
+  }
 
   waterPlants(plantId) {
     if (plantId.length) {
-      api.waterOnePlant(plantId)
+      return api.waterOnePlant(plantId)
         .then(function (waterEvent) {
-          let messageText = "Plant Watered!";
-          this.setState({ waterEvents: waterEvent, messageText: messageText });
-        }.bind(this))
+          return waterEvent;
+        })
     } else {
-      api.waterAllPlants()
+      return api.waterAllPlants()
         .then(function (waterEvents) {
-          let messageText = "All your plants have been watered!";
-          this.setState({ waterEvents: waterEvents, messageText: messageText });
-        }.bind(this))
+          return waterEvents;
+        })
     }
   }
 
@@ -41,13 +53,13 @@ class App extends React.Component {
     return (
       <div>
         <Buttons
-          waterPlants={this.waterPlants} />
+          waterPlants={this.handlePlantWatering} />
         <Message
           messageText={this.state.messageText} />
         <Dashboard
           plants={this.state.plants}
           waterEvents={this.state.waterEvents}
-          waterPlant={this.waterPlants}
+          waterPlant={this.handlePlantWatering}
         />
       </div>
     )
