81566599670adaf733180ba432ebe2d74faec51d
Brett Goss <goss.brett@gmail.com>
2018-11-01 08:02:32 -0700

    Add method for handing legacy passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,33 +1,24 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :plants
 
   validates :name, presence: true, length: { minimum: 2 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   # validates :password, presence: true, length: { in: 4..20 }
 
-  def self.authenticate_with_credentials(email, password)
-    email = email.strip
-    user = User.where('email = ?', email.downcase).first
-
-    if user
-      if user.authenticate(password)
-        user
-      end
+  # Because we have some old legacy users in the database, we need to override Devises method for checking if a password is valid.
+  # We first ask Devise if the password is valid, and if it throws an InvalidHash exception, we know that we're dealing with a
+  # legacy user, so we check the password against the SHA1 algorithm that was used to hash the password in the old database.
+  alias :devise_valid_password? :valid_password?
+  def valid_password?(password)
+    begin
+      devise_valid_password?(password)
+    rescue BCrypt::Errors::InvalidHash
+      Digest::SHA1.hexdigest(password) == encrypted_password
     end
-  end
-
-  def self.has_thristy_plant
-    list = User.where(
-      id: Plant.select('user_id').where(
-        id: WaterEvent.select('plant_id').where(
-          "watered = ? AND water_date < ?",
-          true, (2.days.ago)
-        )
-      )
-    )
-    puts list.inspect
-    list
   end
 end
 # or .collect return associated user. list of users thats has thirsty plants
