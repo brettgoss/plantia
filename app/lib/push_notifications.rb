a11686fc2c4f45a4c57f09ddf6a69bca88d10216
Brett Goss <goss.brett@gmail.com>
2018-11-04 01:37:52 -0700

    Handle trying to send to a user no longer subscribed

--- app/lib/push_notifications.rb
+++ app/lib/push_notifications.rb
@@ -2,19 +2,26 @@ module PushNotifications
   require 'json'
   def send_webpush_notification(params)
     Rails.logger.info "Sending push notification from #{params.inspect}"
-    Webpush.payload_send(
-      message: params[:message],
-      endpoint: params[:subscription][:endpoint],
-      p256dh: params[:subscription][:keys][:p256dh],
-      auth: params[:subscription][:keys][:auth],
-      vapid: {
-        subject: "mailto:plantiapp@gmail.com",
-        public_key: ENV['VAPID_PUBLIC_KEY'],
-        private_key: ENV['VAPID_PRIVATE_KEY']
-      },
-      ssl_timeout: 5, # value for Net::HTTP#ssl_timeout=, optional
-      open_timeout: 5, # value for Net::HTTP#open_timeout=, optional
-      read_timeout: 5 # value for Net::HTTP#read_timeout=, optional
-    )
+    begin
+      @endpoint = params[:subscription][:endpoint]
+      Webpush.payload_send(
+        message: params[:message],
+        endpoint: params[:subscription][:endpoint],
+        p256dh: params[:subscription][:keys][:p256dh],
+        auth: params[:subscription][:keys][:auth],
+        vapid: {
+          subject: "mailto:plantiapp@gmail.com",
+          public_key: ENV['VAPID_PUBLIC_KEY'],
+          private_key: ENV['VAPID_PRIVATE_KEY']
+        },
+        ssl_timeout: 5, # value for Net::HTTP#ssl_timeout=, optional
+        open_timeout: 5, # value for Net::HTTP#open_timeout=, optional
+        read_timeout: 5 # value for Net::HTTP#read_timeout=, optional
+      )
+    rescue Webpush::InvalidSubscription => ex
+      puts @endpoint
+      puts "Rescued"
+      puts ex
+    end
   end
 end
