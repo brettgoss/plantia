33aa6954b71663e35cb96f6084a8a2a8a0226073
Brett Goss <goss.brett@gmail.com>
2018-11-03 21:15:04 -0700

    Use has_many through to link users and water_events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :plants
+  has_many :water_events, through: :plants
 
   validates :name, presence: true, length: { minimum: 2 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
