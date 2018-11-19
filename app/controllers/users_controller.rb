352b95439c9eb359667590b34225fb240ba3a8a1
Brett Goss <goss.brett@gmail.com>
2018-11-19 00:14:00 -0800

    Track notification subscriptions and signups in GA

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
   helper UsersHelper
-  # User Page
+  # User Profile Page
   def index
     if !current_user
       redirect_to(root_path)
@@ -9,27 +9,5 @@ class UsersController < ApplicationController
       @user = current_user
       @plants = @user.plants.all
     end
-  end
-
-  # Signup Logic
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to(dashboard_index_path)
-    else
-      redirect_to(root_path)
-    end
-
-  end
-
-  def update
-    current_user.save!
-    redirect_to '/plants'
-  end
-
-private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
