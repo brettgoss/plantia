b3247f5e71e3cb25c686b6c44eee30231e85b8dc
Brett Goss <goss.brett@gmail.com>
2019-01-19 19:13:29 -0800

    Handle 404 in BaseController

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,8 +4,13 @@ class Api::V1::BaseController < ApplicationController
   protect_from_forgery with: :null_session
 
   before_action :destroy_session
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def destroy_session
     request.session_options[:skip] = true
+  end
+
+  def not_found
+    render json: { status: 404, message: 'Not found'}
   end
 end
