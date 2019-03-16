41ca179cd629716e0982ce3a69c1277e4bfdbd06
Brett Goss <goss.brett@gmail.com>
2019-03-16 14:44:49 -0700

    Move dashboard controller test to RSpec

---
+++ spec/controllers/dashboard_controller_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DashboardController, type: :controller do
+  before do
+    @user = FactoryGirl.create :user
+  end
+
+  describe 'GET index' do
+    context 'when the user is not logged in' do
+      it 'should redirect to the login page' do
+        get :index
+        expect(response).to redirect_to('/login')
+      end
+    end
+
+    context 'when the user is logged in' do
+      it 'should show the dashboard' do
+        login_as(@user)
+        get :index
+        expect(response).to render_template(:index)
+      end
+    end
+  end
+end
