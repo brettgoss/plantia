6dd87caee00f0841a908ffab3a7904686a0a9a1d
Brett Goss <goss.brett@gmail.com>
2019-03-17 16:49:25 -0700

    Remove dead code from application and users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
   helper UsersHelper
   # User Profile Page
   def index
-    if !current_user
-      redirect_to(root_path)
-    else
-      @user = current_user
-      @plants = @user.plants.all
-    end
+    return unless current_user
+
+    @user = current_user
+    @plants = @user.plants.all
   end
 end
