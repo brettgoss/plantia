ca12ec6e6d913a3488bc4a4a4dc3836077961c63
Brett Goss <goss.brett@gmail.com>
2019-03-20 09:40:23 -0700

    Refactor Analytics service to better handle errors

--- app/services/google_analytics_service.rb
+++ app/services/google_analytics_service.rb
@@ -1,23 +1,36 @@
+# frozen_string_literal: true
+
 class GoogleAnalyticsService
   require 'http'
 
   def event(category, action, client_id = '555')
     return unless ENV['GA_TRACKING_ID'].present?
 
-    params = {
-      :v   => 1,
-      :tid => ENV['GA_TRACKING_ID'],
-      :cid => client_id,
-      :t   => "event",
-      :ec  => category,
-      :ea  => action,
-    }
+    @params = get_params(category, action, client_id)
 
-    begin
-      AnalyticsWorker.perform_async(params)
-      return true
-    rescue  HTTP::Error => ex
-      return false
-    end
+    send_analytics_event
+  end
+
+  private
+
+  def send_analytics_event
+    AnalyticsWorker.perform_async(@params)
+    true
+  rescue HTTP::Error
+    Rails.logger.error('Failed to connect to Google Analytics')
+    false
+  rescue Redis::CannotConnectError
+    Rails.logger.error('Failed to connect to Redis')
+  end
+
+  def get_params(category, action, client_id)
+    {
+      v: 1,
+      tid: ENV['GA_TRACKING_ID'],
+      cid: client_id,
+      t: 'event',
+      ec: category,
+      ea: action
+    }
   end
 end
