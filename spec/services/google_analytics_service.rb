dd06c61ad70b371afe7adc410ef7d98bd42d10c6
Brett Goss <goss.brett@gmail.com>
2019-03-20 10:39:36 -0700

    Test if errors are properly handled when analytics fails

--- spec/services/google_analytics_service.rb
+++ spec/services/google_analytics_service.rb
@@ -3,13 +3,57 @@
 require 'rails_helper'
 
 RSpec.describe GoogleAnalyticsService do
-  describe 'When an exception occurs' do
+  before do
+    @params = %w[testing test 12345]
+  end
+
+  describe 'when an exception occurs' do
     it 'should not impact production if Redis is down' do
-      # TODO: Implement this method
+      allow(AnalyticsWorker).to receive(:perform_async).and_raise(Redis::CannotConnectError)
+
+      expect do
+        response = GoogleAnalyticsService.new.event(*@params)
+        expect(response).to be(false)
+      end.to_not raise_error
     end
 
     it 'should not impact production if GoogleAnalytics is down' do
-      # TODO: Implement this method
+      allow(AnalyticsWorker).to receive(:perform_async).and_raise(HTTP::Error)
+
+      expect do
+        response = GoogleAnalyticsService.new.event(*@params)
+        expect(response).to be(false)
+      end.to_not raise_error
+    end
+
+    it 'should not do anything with no analytics ID' do
+      ENV['GA_TRACKING_ID'] = ''
+
+      expect do
+        response = GoogleAnalyticsService.new.event(*@params)
+        expect(response).to be(false)
+      end.to_not raise_error
+    end
+  end
+
+  describe 'when there are no exceptions' do
+    it 'should perform an analytics event successfully' do
+      ENV['GA_TRACKING_ID'] = 'UA-12345'
+      allow(AnalyticsWorker).to receive(:perform_async)
+
+      expect do
+        response = GoogleAnalyticsService.new.event(*@params)
+        expect(response).to be(true)
+      end.to_not raise_error
+
+      expect(AnalyticsWorker).to have_received(:perform_async).with(
+        cid: '12345',
+        ea: 'test',
+        ec: 'testing',
+        t: 'event',
+        tid: ENV['GA_TRACKING_ID'],
+        v: 1
+      )
     end
   end
 end
