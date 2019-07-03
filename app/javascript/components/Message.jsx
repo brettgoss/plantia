7126de42793597d3dca81c9c812ba3af4684f3bc
Brett Goss <goss.brett@gmail.com>
2019-07-03 16:30:56 -0700

    Use react hooks

--- app/javascript/components/Message.jsx
+++ app/javascript/components/Message.jsx
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
-class Message extends Component {
-  componentDidUpdate() {
+function Message({ messageText }) {
+  useEffect(() => {
     // TODO: Switch this to React Animations: https://reactjs.org/docs/animation.html
     $('.message')
       .show()
       .delay(1500)
       .fadeOut();
-  }
+  });
 
-  render() {
-    return (
-      <div id="messageWrapper">
-        <div className="message">{this.props.messageText}</div>
-      </div>
-    );
-  }
+  return (
+    <div id="messageWrapper">
+      <div className="message">{messageText}</div>
+    </div>
+  );
 }
 
 export default Message;
