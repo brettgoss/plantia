55bed1598398418d7b634e01379570b6203671c2
Brett Goss <goss.brett@gmail.com>
2019-07-03 18:07:30 -0700

    DRY ButtonBar and Dashboard

--- app/javascript/components/ButtonBar.jsx
+++ app/javascript/components/ButtonBar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 function ButtonBar({ handlePlantWatering }) {
+  const handleSubmit = () => handlePlantWatering();
+
   return (
     <div id="buttonBar">
       <a className="button" href="/plants">
         See All
       </a>
-      <button className="button" onClick={() => handlePlantWatering()}>
+      <button className="button" onClick={handleSubmit}>
         Water All
       </button>
       <a className="button" href="/plants/new">
--- app/javascript/components/Dashboard.jsx
+++ app/javascript/components/Dashboard.jsx
@@ -3,27 +3,23 @@ import Card from './Card';
 
 function getWaterEventByPlantId(waterEvents, plantId) {
   return waterEvents
-    .filter(waterEvent => {
-      if (waterEvent.plant_id == plantId) {
-        return waterEvent;
-      }
-    })
+    .filter(waterEvent => waterEvent.plant_id == plantId && waterEvent)
     .shift();
 }
 
 function Dashboard({ plants, waterEvents, waterPlant }) {
   return (
     <div className="wrapper">
-      {plants.map(plant => {
-        return (
-          <Card
-            key={plant.id}
-            plant={plant}
-            waterEvent={waterEvents && getWaterEventByPlantId(waterEvents, plant.id)}
-            waterPlant={waterPlant}
-          />
-        );
-      })}
+      {plants.map(plant => (
+        <Card
+          key={plant.id}
+          plant={plant}
+          waterEvent={
+            waterEvents && getWaterEventByPlantId(waterEvents, plant.id)
+          }
+          waterPlant={waterPlant}
+        />
+      ))}
     </div>
   );
 }
